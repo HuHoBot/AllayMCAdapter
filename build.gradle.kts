b7cc4e9fb8ef6e36a7145dbebb1e9ecbe8b16f60
Cdm2883 <Cdm2883@hotmail.com>
2025-02-04 23:44:57 +0800

    build: run server testing plugin task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java-library")
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -30,4 +32,27 @@ dependencies {
 
 tasks.shadowJar {
     archiveClassifier = "shaded"
+}
+
+tasks.register<Copy>("runServer") {
+    outputs.upToDateWhen { false }
+    dependsOn("shadowJar")
+    val launcherRepo = "https://raw.githubusercontent.com/AllayMC/AllayLauncher/refs/heads/main/scripts"
+    val cmdWin = "Invoke-Expression (Invoke-WebRequest -Uri \"${launcherRepo}/install_windows.ps1\").Content"
+    val cmdLinux = "wget -qO- ${launcherRepo}/install_linux.sh | bash"
+    val cwd = layout.buildDirectory.file("launcher").get().asFile.apply { mkdirs() }
+
+    val shadowJar = tasks.named("shadowJar", ShadowJar::class).get()
+    from(shadowJar.archiveFile.get().asFile)
+    into(cwd.resolve("plugins").apply { mkdirs() })
+
+    val isWindows = System.getProperty("os.name").startsWith("Windows")
+    fun launch() = exec {
+        workingDir = cwd
+        if (isWindows) commandLine("powershell", "-Command", cmdWin)
+        else commandLine("sh", "-c", cmdLinux)
+    }
+
+    // https://github.com/gradle/gradle/issues/18716  // kill it manually by click X...
+    doLast { launch() }
 }
